e3a2054ec9f17e1bd282b3ff2db5f1b7f8d8fc50
Florian Lauch <florian.lauch@epitech.eu>
2023-05-22 14:28:26 +0200

    feat(repo): setup server folder

---
+++ server/include/server.h
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2023
+** Zappy
+** File description:
+** server.h
+*/
+
+#ifndef SERVER_H_
+    #define SERVER_H_
+
+int zappy_server(int argc, char **argv);
+
+#endif
---
+++ server/tests/test_server.c
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2023
+** Zappy
+** File description:
+** test_utils.c
+*/
+
+#include <criterion/criterion.h>
+#include <stdio.h>
+
+Test(a, b)
+{
+    printf("Test suite not implemented yet");
+}
