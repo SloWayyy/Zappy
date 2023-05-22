2297cccd7cd7f273d203b0211fb19a85598662e0
Mehdi Djendar <mehdi.djendar@epitech.eu>
2023-05-22 22:00:53 +0200

    feat: add display help

--- ai/src/main.py
+++ ai/src/main.py
@@ -6,10 +6,18 @@ from ai.src.game import game_loop
 from ai.src.handle_args import pars_args, BadArgumentException
 from ai.src.handle_packets import get_socket, BadConnectionException
 
+def display_help():
+    print("USAGE: ./zappy_ai -p port -n name -h machine \
+          \n        port    is the port number \
+          \n        name    is the name of the team \
+          \n        machine is the name of the machine; localhost by default")
+
 def main():
+    if len(sys.argv) == 2 and sys.argv[1] == "--help":
+        display_help()
+        sys.exit(0)
     try:
         args = pars_args()
-        print (args.address)
         connexion, adresse = get_socket(args.port, args.address if args.address is not None else "")
         game_loop(args, connexion, adresse)
         sys.exit(0)
