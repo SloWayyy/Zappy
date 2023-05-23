fdc7ca83d1c58745061c7bbff1e93e4e090b39b1
Taha ALANI <taha.alani@epitech.eu>
2023-05-24 00:53:30 +0200

    fix(camera): set good view of camera when you spawn hohoho

--- client/src/graphical.cpp
+++ client/src/graphical.cpp
@@ -60,8 +60,8 @@ void graphical::draw()
 
 void graphical::setCamera()
 {
-    camera.position = (Vector3){ 10.0f, 10.0f, 10.0f }; // Camera position
-    camera.target = (Vector3){ 0.0f, 0.0f, 0.0f };      // Camera looking at point
+    camera.position = (Vector3){ 0.0f, 10.0f, 10.0f }; // Camera position
+    camera.target = (Vector3){ 10.0f, 2.0f, 10.0f };      // Camera looking at point
     camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };          // Camera up vector (rotation towards target)
     camera.fovy = 45.0f;                                // Camera field-of-view Y
     camera.projection = CAMERA_PERSPECTIVE;             // Camera projection type
