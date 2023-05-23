070ee1ba273321a85ca80d3c73a28765c7088e26
Mehdi Djendar <mehdi.djendar@epitech.eu>
2023-05-24 00:28:33 +0200

    feat(install): add .sh for install raylib in the install directory

---
+++ install/raylib_install.sh
@@ -0,0 +1,25 @@
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+echo "Installing RAYLIB for Fedora..."
+
+# Install dependencies
+dnf install -y cmake make gcc g++ libX11-devel libXrandr-devel libXcursor-devel libatomic libXi-devel mesa-libGL-devel mesa-libGLU-devel
+
+# Download RAYLIB and unzip
+mkdir -p raylib
+cd raylib/
+wget https://github.com/raysan5/raylib/archive/refs/heads/master.zip
+unzip master.zip
+cd raylib-master
+
+# Install RAYLIB
+cmake .
+make
+sudo make install
+
+# Remove temp files
+
+echo "RAYLIB successfully installed!"
