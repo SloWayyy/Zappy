1792d57cc1b3e93f2932f1647b6f88a88e98faab
Florian Lauch <florian.lauch@epitech.eu>
2023-05-31 14:29:29 +0200

    fix: merge, coding style

--- server/include/args.h
+++ server/include/args.h
@@ -20,12 +20,12 @@ bool check_positive(char const *str, char *option, int *storage);
 typedef int (options_handler_t) \
     (int argc, char const *argv[], options_t *options, int index);
 
-int port_handler(int argc, char const *argv[], options_t *options, int index);
-int width_handler(int argc, char const *argv[], options_t *options, int index);
-int height_handler(int argc, char const *argv[], options_t *options, int index);
-int names_handler(int argc, char const *argv[], options_t *options, int index);
-int clients_handler(int argc, char const *argv[], options_t *options, int index);
-int freq_handler(int argc, char const *argv[], options_t *options, int index);
+int port_handler(int argc, char const *argv[], options_t *options, int idx);
+int width_handler(int argc, char const *argv[], options_t *options, int idx);
+int height_handler(int argc, char const *argv[], options_t *options, int idx);
+int names_handler(int argc, char const *argv[], options_t *options, int idx);
+int clients_handler(int argc, char const *argv[], options_t *options, int idx);
+int freq_handler(int argc, char const *argv[], options_t *options, int idx);
 
 typedef struct option {
     char const *shortOpt;
