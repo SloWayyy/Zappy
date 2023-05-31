4c8f47b75e6e6273e805f3c1b5b45ceeefee3043
Mehdi Djendar <mehdi.djendar@epitech.eu>
2023-06-01 00:12:30 +0200

    feat(player): add boss in the start of the game with check if already exist

--- ai/src/game.py
+++ ai/src/game.py
@@ -1,18 +1,27 @@
 import socket
 
+from anyio import sleep
+
 from ai.src.player import Player, EnumObject, EnumHeader
 
-def game_loop(sock: socket.socket, name: str):
+def game_init(sock: socket.socket, name: str) -> Player:
     player = Player(sock, name)
-    # player.move()
-    # player.turn(Direction.RIGHT)
-    # player.look()
-    player.broadcast(EnumHeader.BOSS.value + " test")
-    player.wait_answer()
-    print("\n\n")
-    player.broadcast("$2$ test2")
-    player.wait_answer()
+    player.broadcast(EnumHeader.ASKBOSS.value + " Who\n")
+    player.look()
+    player.take(EnumObject.FOOD.value)
 
-    # player.take(food)
-    # player.broadcast("test")
+    print('\n\n')
+
+    if player.boss == 0:
+        print("already a boss\n")
+    else:
+        player.boss = 1
+        print("I'm the new boss\n")
+
+    player.wait_answer()
+    return player
+
+
+def game_loop(sock: socket.socket, name: str):
+    player = game_init(sock, name)
     sock.close()
--- ai/src/player.py
+++ ai/src/player.py
@@ -15,7 +15,8 @@ class EnumObject (Enum):
     THYSTAME = "thystame"
 
 class EnumHeader (Enum):
-    BOSS = "$1$"
+    ASKBOSS = "$1$"
+    IMBOSS = "$2$"
 
 class Direction(Enum):
     RIGHT = 0,
@@ -24,20 +25,22 @@ class Direction(Enum):
 class Player:
     def __init__(self, sock: socket.socket, name: str):
         self.sock = sock
+        self.boss = -1
         self.map_broadcast : dict = {}
         if (self.connection(name) == False):
             ErrorConnection("Error: connection failed")
 
     def handle_broadcast(self, donnees: str):
         for i in donnees:
-            x = re.findall("^message ([0-8]), \$([0-9])\$ (\w+)$", i)
+            x = re.findall("^message ([0-8]), (\$[0-9]\$) (\w+)$", i)
             if len(x) != 0:
-                self.map_broadcast.update({int(x[0][0]) : (int(x[0][1]), x[0][2])})
+                if  x[0][1] == EnumHeader.ASKBOSS.value and self.boss == 1:
+                    self.broadcast(EnumHeader.IMBOSS.value + " IMBOSS")
+                elif x[0][1] == EnumHeader.IMBOSS.value and self.boss == -1:
+                    self.boss = 0
+                else:
+                    self.map_broadcast.update({int(x[0][0]) : (x[0][1], x[0][2])})
                 donnees.remove(i)
-
-            for key, value in self.map_broadcast.items():
-                print("map: key =", key, ", value =", value)
-        # print("Données restantes :", donnees)
 
     def wait_answer_debug(self):
         donnees = self.sock.recv(1024)
@@ -59,7 +62,7 @@ class Player:
         donnees = donnees.split(b'\n')
         for i, x in enumerate(donnees):
             donnees[i] = x.decode()
-        print("Données reçues :", donnees)
+        # print("Données reçues :", donnees)
         self.handle_broadcast(donnees)
         if len(donnees) <= 1:
             return self.wait_answer()
@@ -104,7 +107,8 @@ class Player:
 
     def broadcast(self, message: str):
         self.sock.send(("Broadcast " + message + "\n").encode())
-        if (self.wait_answer()[0] == "ko"):
+        result = self.wait_answer()[0]
+        if (result == "ko"):
             return False
         return True
 
@@ -128,7 +132,7 @@ class Player:
         return True
 
     def take(self, enum: EnumObject):
-        self.sock.send(("Take " + enum.value + "\n").encode())
+        self.sock.send(("Take " + enum + "\n").encode())
         if (self.wait_answer()[0] == "ko"):
             return False
         return True
