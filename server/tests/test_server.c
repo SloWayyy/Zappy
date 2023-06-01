09ba24e48fe2297863f6ec6b3458b52f1fc5d16f
Jonathan Yakan <jonathan.yakan@epitech.eu>
2023-06-01 23:20:29 +0200

    feat: unit test on the function port_handler

--- server/tests/test_server.c
+++ server/tests/test_server.c
@@ -37,7 +37,7 @@ Test(check_arguments, unknown_opt, .init=redirect_all)
 Test(check_arguments, only_team_name)
 {
     options_t options;
-    cr_assert_eq(check_arguments(4, (char const *[]){"./zappy_server", "-n", "toto", "tata", "-p", "5555", NULL}, &options), true);
+    cr_assert_eq(check_arguments(4, (char const *[]){"./zappy_server", "-n", "tfoto", "tatva"}, &options), true);
 }
 
 Test(check_arguments, default_values)
@@ -57,3 +57,35 @@ Test(check_arguments, invalid_port, .init=redirect_all)
     options_t options;
     cr_assert_eq(check_arguments(5, (char const *[]){"./zappy_server", "-p", "toto", "-n", "toto", "tata", NULL}, &options), false);
 }
+
+Test(check_arguments, invalid_map_dimension, .init=redirect_all)
+{
+    options_t options;
+    check_arguments(6, (char const *[]){"./zappy_server", "-x", "10", "-n", "toto", "tata", NULL}, &options);
+    cr_assert_stderr_eq_str("Error: Map dimensions are not set\n");
+}
+
+int port_handler(int argc, char const *argv[], options_t *options, int idx);
+Test(port_handler, invalid_port_nbr_arg, .init=redirect_all)
+{
+    options_t options;
+    port_handler(5, (char const *[]){"./zappy_server", "-n", "toto", "tata", "-p"}, &options, 4);
+    cr_assert_stderr_eq_str("Error: -p requires an argument\n");
+}
+
+Test(port_handler, port_already_set, .init=redirect_all)
+{
+    options_t options;
+    check_arguments(8, (char const *[]){"./zappy_server", "-n", "toto", "tata", "-p", "10", "-p", "15"}, &options);
+    cr_assert_stderr_eq_str("Error: Port already set\n");
+}
+
+Test(port_handler, port_too_big)
+{
+    options_t options;
+    cr_assert_eq(port_handler(6, (char const *[]){"./zappy_server", "-n", "toto", "tata", "-p", "65536"}, &options, 4), -1);
+}
+
+
+// height_handler a faire
+// clients_handler a faire
