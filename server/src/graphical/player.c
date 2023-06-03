10d1da64adb78e9e328380c80e000faf53228a0e
Florian Lauch <florian.lauch@epitech.eu>
2023-06-03 17:22:04 +0200

    fix: segfault

--- server/src/graphical/player.c
+++ server/src/graphical/player.c
@@ -12,7 +12,7 @@
 void tna_handler(server_t *server, client_t *client, char *line)
 {
     (void) line;
-    for (size_t i = 0; server->options->names != NULL; i++) {
+    for (size_t i = 0; server->options->names[i] != NULL; i++) {
         append_buffer(client->buffer, "%s %s%s", GRAPHICAL_TEAM_NAME, \
             server->options->names[i], LINE_BREAK);
     }
