2ead30db6833289d030d48b7d0a1a7678d921ee1
Jonathan Yakan <jonathan.yakan@epitech.eu>
2023-06-05 14:43:38 +0200

    feat(server): add command bct, return the content of a tile

--- server/src/graphical/map.c
+++ server/src/graphical/map.c
@@ -5,8 +5,11 @@
 ** map_answers.c
 */
 
+#include <string.h>
+
 #include "constants.h"
 #include "graphical.h"
+#include "server.h"
 #include "util.h"
 #include "types.h"
 
@@ -14,4 +17,39 @@ void msz_handler(server_t *server, client_t *client)
 {
     append_buffer(client->buffer, "%s %d %d%s", GRAPHICAL_MAP_SIZE, \
         server->options->width, server->options->height, LINE_BREAK);
+}
+
+static bool check_map_coord(server_t *server, const int x, const int y,
+client_t *client)
+{
+    if (x < 0 || x >= server->options->width || y < 0 || \
+        y >= server->options->height) {
+        append_buffer(client->buffer, "%s%s", GRAPHICAL_COMMAND_PARAMETER, 
+            LINE_BREAK);
+        return false;
+        }
+    return true;
+}
+
+void bct_handler(server_t *server, client_t *client)
+{
+    char *intput_x = strtok(NULL, " ");
+    char *intput_y = strtok(NULL, " ");
+    int x = 0;
+    int y = 0;
+
+    if (intput_x == NULL || intput_y == NULL || strtok(NULL, " ") != NULL ||
+        !check_number(intput_x, &x) || !check_number(intput_y, &y)) {
+        append_buffer(client->buffer, "%s\n", GRAPHICAL_COMMAND_PARAMETER);
+        return;
+    }
+    if (!check_map_coord(server, x, y, client))
+        return;
+    append_buffer(client->buffer, "%s %zu %zu %zu %zu %zu %zu %zu %zu%s", \
+        GRAPHICAL_TILE_CONTENT, x, y, server->zappy->map[y][x].resources[0], \
+        server->zappy->map[y][x].resources[1], \
+        server->zappy->map[y][x].resources[2], \
+        server->zappy->map[y][x].resources[3], \
+        server->zappy->map[y][x].resources[4], \
+        server->zappy->map[y][x].resources[5], LINE_BREAK);
 }
