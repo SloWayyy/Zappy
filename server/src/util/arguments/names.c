2c6a8199fb5c9ba31a171f61ed63c0839d27cff0
Jonathan Yakan <jonathan.yakan@epitech.eu>
2023-06-05 16:12:28 +0200

    style: refacto code, using macro now

--- server/src/util/arguments/names.c
+++ server/src/util/arguments/names.c
@@ -10,12 +10,13 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include "graphical.h"
 #include "args.h"
 #include "types.h"
 
 static bool is_valid(const char **teams, int size, char const *name)
 {
-    if (!strcmp("GRAPHIC", name)) {
+    if (strcmp(GRAPHICAL_IDENTIFIER, name) == 0) {
         fprintf(stderr, "Error: The team can't have this name.\n");
         return true;
     }
