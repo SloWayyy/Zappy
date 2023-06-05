74d702511bb30ef852abb7c6220b210be2065e14
kevin nadarajah <kevin.nadarajah@epitech.eu>
2023-06-05 12:40:39 +0200

    feat(class Parser) get information from zappy_gui and store into variables in this class with error handling

--- client/src/Main.cpp
+++ client/src/Main.cpp
@@ -8,9 +8,17 @@
 #include <memory>
 #include "encapsulation/Raylibcpp.hpp"
 #include "src/Core/Core.hpp"
+#include "src/Parser/Parser.hpp"
 
-int main(void)
+int main(int ac, char **av)
 {
+    try {
+        Parser parser(ac, av);
+        parser.handleArgs();
+    } catch (const Parser::ParserException &e) {
+        std::cerr << e.what() << std::endl;
+        return 84;
+    }
     Core core = Core();
     core.run();
     return 0;
---
+++ client/src/Parser/Parser.hpp
@@ -0,0 +1,49 @@
+/*
+** EPITECH PROJECT, 2023
+** Zappy
+** File description:
+** Parser
+*/
+
+#ifndef PARSER_HPP_
+    #define PARSER_HPP_
+
+    #include <string>
+    #include <vector>
+    #include <sstream>
+
+class Parser {
+    public:
+        class ParserException : public std::exception {
+            public:
+                ParserException(std::string const &message);
+                ~ParserException() = default;
+                const char *what() const noexcept;
+            private:
+                std::string _message;
+        };
+        Parser(int ac, char **av);
+        size_t getPort(void) const;
+        std::string getMachine(void) const;
+        void handleArgs(void);
+        void setHelp(void);
+        template<typename T>
+        T parseArgs(const std::string &str) {
+            std::stringstream stream(str);
+            T value;
+
+            stream >> value;
+            if (stream.fail() || !stream.eof()) {
+                throw ParserException("Invalid argument\n" + this->_usage);
+            }
+            return value;
+        }
+        ~Parser() = default;
+    private:
+        std::vector<std::string> _av;
+        size_t _port;
+        std::string machine;
+        std::string _usage;
+};
+
+#endif
