725a42f71b67c49b865dd7d3a7e29acc5a81ecfd
Jonathan Yakan <jonathan.yakan@epitech.eu>
2023-06-05 15:09:58 +0200

    feat(server): check when received arg msz and tna if there is others arguments or not

--- server/src/graphical/team_name.c
+++ server/src/graphical/team_name.c
@@ -14,6 +14,11 @@
 
 void tna_handler(server_t *server, client_t *client)
 {
+    if (strtok(NULL, " ") != NULL) {
+        append_buffer(client->buffer, "%s%s", GRAPHICAL_COMMAND_PARAMETER, \
+            LINE_BREAK);
+        return;
+    }
     for (size_t i = 0; server->options->names[i] != NULL; i++) {
         append_buffer(client->buffer, "%s %s%s", GRAPHICAL_TEAM_NAME, \
             server->options->names[i], LINE_BREAK);
