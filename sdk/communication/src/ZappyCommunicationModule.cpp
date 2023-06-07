d29aac1cfc9df3d5a565034b75d1e639a6928afe
Florian Lauch <florian.lauch@epitech.eu>
2023-06-07 12:15:02 +0200

    feat(sdk): connection and draft communication

---
+++ sdk/communication/src/ZappyCommunicationModule.cpp
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2023
+** Zappy
+** File description:
+** SDK
+*/
+
+#include "ZappyCommunicationModule.hpp"
+
+zappy::sdk::ZappyCommunicationModule::ZappyCommunicationModule(): ACommunicationModule() {
+
+}
+
+void zappy::sdk::ZappyCommunicationModule::connectAsGraphical() {
+
+}
