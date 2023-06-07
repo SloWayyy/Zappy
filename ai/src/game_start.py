1f2bf1c6ca39cf0a3e2a149ad84da80f88fcc62a
Sloyi <rayan.es-seffar@epitech.eu>
2023-06-07 15:49:32 +0200

    style: remove debug print

--- ai/src/game_start.py
+++ ai/src/game_start.py
@@ -23,29 +23,24 @@ def locate_boss(player: Player):
 
     if (player.pos_boss == 0):
         player.broadcast(EnumHeader.IMHERE.value + " IMHERE\n")
-        print("IMHERE\n")
         return True
     elif (player.pos_boss == 1 or player.pos_boss == 2 or player.pos_boss == 8):
         player.move()
         player.wait_broadcast()
         player.pos_boss = -1
-        print("IMOVE\n")
     elif (player.pos_boss == 4 or player.pos_boss == 5 or player.pos_boss == 3):
-        print(player.turn(EnumDirection.LEFT))
+        player.turn(EnumDirection.LEFT)
         player.wait_broadcast()
         player.pos_boss = -1
-        print("LEFT\n")
     elif (player.pos_boss == 6 or player.pos_boss == 7):
-        print(player.turn(EnumDirection.RIGHT))
+        player.turn(EnumDirection.RIGHT)
         player.wait_broadcast()
         player.pos_boss = -1
-        print("RIGHT\n")
     return False
 
 def join_boss(player: Player):
     if player.boss == 1:
         while (player.nbr_ai > 0):
-            print("je gueule {}\n".format(player.nbr_ai))
             player.broadcast(EnumHeader.IMBOSS.value + " IMBOSS\n")
     else:
         while (locate_boss(player) == False):
