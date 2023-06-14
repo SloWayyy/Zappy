5b8611024cf20688f3dfa555651560923b6791b8
Jonathan Yakan <jonathan.yakan@epitech.eu>
2023-06-14 20:52:28 +0200

    feat(ai): add ordre dump item in the ordre square_collect

--- ai/src/game.py
+++ ai/src/game.py
@@ -1,26 +1,9 @@
 import socket
 
 from ai.src.game_start import find_boss, join_boss
-from ai.src.player import Player, EnumObject
+from ai.src.player import Player
 from ai.src.ordre.square_collect import square_collect
 from ai.src.players.routine_boss import routine_boss
-
-def dump_item(player: Player):
-    inventory = player.inventory()
-    for i in range(1, len(inventory)):
-        for _ in range(0, inventory[i]):
-            if i == 1:
-                player.set(EnumObject.LINEMATE)
-            if i == 2:
-                player.set(EnumObject.DERAUMERE)
-            if i == 3:
-                player.set(EnumObject.SIBUR)
-            if i == 4:
-                player.set(EnumObject.MENDIANE)
-            if i == 5:
-                player.set(EnumObject.PHIRAS)
-            if i == 6:
-                player.set(EnumObject.THYSTAME)
 
 def game_loop(sock: socket.socket, args):
     player = Player(sock, args)
--- ai/src/ordre/square_collect.py
+++ ai/src/ordre/square_collect.py
@@ -1,6 +1,6 @@
-from asyncio import sleep
-from ai.src.player import Player, EnumDirection, EnumObject, EnumHeader
+from ai.src.player import Player, EnumDirection
 from ai.src.players.routine_boss import look_item
+from ai.src.ordre.dump_item import dump_item
 
 def first_pattern_ai(list_item: list, player: Player):
     player.move()
@@ -79,4 +79,5 @@ def square_collect(player: Player, orientation: int):
     second_pattern_ai(list_item[2:], player)
     if (return_to_boss(player) == False):
         return False
-    print("(AI) je suis arrivé au boss avec dans mon inventory: ", player.inventory())
+    dump_item(player)
+    print("(AI) je suis arrivé au boss et j'ai déposé mes items")
