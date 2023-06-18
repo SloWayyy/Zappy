f5e7b73095a30d0e3835eefd57715e0111f9068b
Sloyi <rayan.es-seffar@epitech.eu>
2023-06-18 12:48:42 +0200

    feat: add function to create message (preventing some mistake)

--- ai/src/game.py
+++ ai/src/game.py
@@ -2,12 +2,20 @@ from ai.src.player import Player, EnumBoss, EnumHeader, EnumOrder, EnumPriorityO
 from ai.src.group_init import find_boss
 from ai.src.player import ALL
 
+
+def msg_create(sender: Player, receiver: str, header: str, order: str = None, data: str = "Nothing") -> str:
+    if (order != None):
+        return (sender.uuid + " " + header + " " + receiver + " " + order + " " + data)
+    else:
+        return (sender.uuid + " " + header + " " + receiver + " " + data)
+
+
 def boss_routine(player: Player):
     for i in player.array_uuid:
-        player.broadcast(player.uuid + " " + EnumHeader.ORDER.value + " " + i["uuid"] + " " + EnumOrder.JOIN_BOSS.value + " Nothing", False)
-        player.broadcast(player.uuid + " " + EnumHeader.PRIORITY_ORDER.value + " " + i["uuid"] + " " + EnumPriorityOrder.PING.value + " Nothing", False)
+        player.broadcast(msg_create(player, i["uuid"], EnumHeader.ORDER.value, EnumOrder.JOIN_BOSS.value), False)
+        player.broadcast(msg_create(player, i["uuid"], EnumHeader.PRIORITY_ORDER.value, EnumPriorityOrder.PING.value), False)
     while True:
-        player.broadcast(player.uuid + " " + EnumHeader.IMBOSS.value + " " + ALL + " " + "JAJAJA", False)
+        player.broadcast(msg_create(player, ALL, EnumHeader.IMBOSS.value), False)
 
 def game_loop(sock, args):
     player = Player(sock, args)
