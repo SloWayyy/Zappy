1db66acaea4cf86399f04b5e6c6fecf0898abc95
kevin nadarajah <kevin.nadarajah@epitech.eu>
2023-06-22 18:43:20 +0200

    fix(DISCONNECTION): replace msg Because the server is down to The server has stopped

--- client/src/Menu/Disconnect.cpp
+++ client/src/Menu/Disconnect.cpp
@@ -15,7 +15,7 @@ Disconnect::Disconnect(std::shared_ptr<Window> window)
 
     createButtons("EXIT", {width / 50.0f, height / 30.0f}, GOLD, 50, EXIT);
     createText("You have been disconnected", {width / 3.5f, height / 10.0f}, RED, 100);
-    createText("Because the server is down", {width / 2.0f, height / 5.0f}, BLACK, 50);
+    createText("The server has stopped", {width / 2.0f + 100, height / 5.0f}, BLACK, 50);
     createButtons("MENU", {width / 1.2f - 50, height / 3.0f}, GOLD, 50, MENU);
 }
 
